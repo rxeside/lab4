17875601e00bf342523e691c72f81a2aca6aa4a5
rxeside <denis.leuhin@ispring.institute>
2023-04-25 18:50:53 +0300

    touched lab7

--- data/sql/table/post.sql
+++ data/sql/table/post.sql
@@ -3,6 +3,7 @@ CREATE TABLE post
    `post_id`      INT NOT NULL AUTO_INCREMENT,
    `title`        VARCHAR(255) NOT NULL,
    `subtitle`     VARCHAR(255) NOT NULL,
+   `modifier`     VARCHAR(255) NOT NULL,
    `publish_date` VARCHAR(255) NOT NULL,
    `image_url`    VARCHAR(255) NOT NULL,
    `author`       VARCHAR(255) NOT NULL,
